0ab7b9419108ca5644a750f5a330a32e9c079d35
andres <andresgarciar96@gmail.com>
2024-01-26 20:30:28 +0100

    flocking +source loc v1

--- agents/agent_flock.py
+++ agents/agent_flock.py
@@ -9,7 +9,7 @@ from pygame.math import Vector2
 
 # Inherit from Storms agent class
 class FlockingAgent(Agent):
-    def __init__(self, simulation, images, radius=5, speed=2, alignment_factor=1.0,  # Might try the matrix config trick
+    def __init__(self, simulation, images, radius=5, speed=2, alignment_factor=0,  # Might try the matrix config trick
                  cohesion_factor=1.2, separation_factor=0.8, separation_radius=20):
         super().__init__(images, simulation)
         self.radius = radius
@@ -60,7 +60,6 @@ class SourceAgent(FlockingAgent):
     def __init__(self, simulation, images, radius=5, speed=5, alignment_factor=1.0,
                  cohesion_factor=1.2, separation_factor=0.8, separation_radius=20,
                  image_path="images/light_source.jpg", epsilon=0.1):
-        # Call the constructor of the parent class (FlockingAgent)
         super().__init__(simulation, images, radius, speed, alignment_factor,
                          cohesion_factor, separation_factor, separation_radius)
 
@@ -70,11 +69,9 @@ class SourceAgent(FlockingAgent):
         self.epsilon = epsilon
 
     def update(self):
-        # Perform flocking calculations
-        # super().update()
-
         # Reset move to not accumulate velocity
         self.move = Vector2(0, 0)
+        at_source = False
 
         # First version will just round the pos and won't probably be able to move diagonally properly
         pos_x, pos_y = self.pos
@@ -96,8 +93,43 @@ class SourceAgent(FlockingAgent):
                 self.move += final_dir
         else:
             self.move += Vector2(random.randint(-1, 1), random.randint(-1, 1))
+            # if self.gradient[pos_x][pos_y] <= 500:
+            #     self.move += Vector2(random.randint(-1, 1), random.randint(-1, 1))
 
-        self.pos += self.move
+        nearby_agents = list(self.in_proximity_accuracy())
+
+        # If there are neighbors, flock!
+        if nearby_agents:
+            average_velocity = pg.math.Vector2()
+            center_of_mass = pg.math.Vector2()
+            separation_vector = pg.math.Vector2()
+
+            # Build nearby agents vectors
+            for agent, distance in nearby_agents:
+                average_velocity += agent.move
+                center_of_mass += agent.pos
+                if distance < self.separation_radius:
+                    vector_to_agent = agent.pos - self.pos
+                    separation_vector += vector_to_agent.normalize()
+
+            # Calculate average vectors
+            average_velocity /= len(nearby_agents)
+            center_of_mass /= len(nearby_agents)
+
+            # Alignment
+            self.move += average_velocity * self.alignment_factor
+
+            # Cohesion
+            direction_to_center = center_of_mass - self.pos
+            scaled_direction = direction_to_center.normalize() * self.cohesion_factor
+            self.move += scaled_direction
+
+            # Separation
+            self.move -= separation_vector * self.separation_factor
+
+            # Clamp speed and update position
+            self.move.scale_to_length(self.speed)
+            self.pos += self.move
 
     def sense_highest(self, drone_x, drone_y, sensor_range) -> Vector2:
 
