da446e54dfced508f43d037e555ffe2f4fc0e556
Andres <andresgarciar96@gmail.com>
2024-02-29 15:42:53 +0100

    minor corrections

--- tests/force_tests.py
+++ tests/force_tests.py
@@ -41,6 +41,7 @@ def simulate_swarm(n_agents, initial_positions, initial_headings, run_wall_time=
         d_ij_x = xx1 - xx2
         d_ij_y = yy1 - yy2
         d_ij = np.sqrt(np.multiply(d_ij_x, d_ij_x) + np.multiply(d_ij_y, d_ij_y))
+        print(f"Dists: {d_ij[0][1]}")
 
         d_ij[d_ij > sensing_range] = np.inf
         d_ij[d_ij == 0.0] = np.inf
@@ -49,7 +50,7 @@ def simulate_swarm(n_agents, initial_positions, initial_headings, run_wall_time=
 
         forces = -epsilon * (2 * (sigmas[:, np.newaxis] ** 4 / d_ij ** 5) - (sigmas[:, np.newaxis] ** 2 / d_ij ** 3))
         forces[d_ij == np.inf] = 0.0
-        # print(f"Forces: {forces}")
+        print(f"Forces: {forces}")
 
         alignment_coss = np.sum(np.cos(pos_hs))
         alignment_sins = np.sum(np.sin(pos_hs))
@@ -61,9 +62,12 @@ def simulate_swarm(n_agents, initial_positions, initial_headings, run_wall_time=
 
         f_x = alpha * np.sum(np.multiply(forces, np.cos(ij_ang)), axis=1)  # + \
               #int(True) * beta * forces_alignment_x
+        print(f"Fx: {f_x}")
 
         f_y = alpha * np.sum(np.multiply(forces, np.sin(ij_ang)), axis=1) #+ \
               #int(True) * beta * forces_alignment_y
+
+        print(f"Fy: {f_y}")
 
         u = k1 * f_x + 0.05
         u[u > umax_const] = umax_const
@@ -79,25 +83,28 @@ def simulate_swarm(n_agents, initial_positions, initial_headings, run_wall_time=
         pos_ys = pos_ys + final_ys * dt
         pos_hs = wrap_to_pi(pos_hs + w * dt)
 
-        if (not (i % 1)) and (i > 0) and if_plot:
+        if (not (i % 20)) and (i > 0) and if_plot:
             plotter.update_plot(pos_xs, pos_ys, pos_hs)
 
         # Helper Prints
         print(f"Us: {u}")
         print(f"Ws: {w}")
-        print(f"Final x Additions: {final_xs}")
-        print(f"Final y Additions: {final_ys}")
+        print(f"x Additions: {final_xs}")
+        print(f"y Additions: {final_ys}")
+        print(f"New xs: {pos_xs}")
+        print(f"New ys: {pos_ys}")
         print(f"Final Headings: {pos_hs}")
+        print("---------------------------")
 
         if i == 1:
             time.sleep(5)
 
 
 if __name__ == "__main__":
-    n_agents = 2
-    pos_xs = np.array([0, 1])
-    pos_ys = np.array([0, 0])
-    pos_hs = np.array([0, 0])
+    n_agents = 4
+    pos_xs = np.array([-3, -2, -2, -2.1])
+    pos_ys = np.array([-2.5, -2, -3, -2.4])
+    pos_hs = np.array([0.5, 0.5, 0.5, 0.5])
     initial_positions = np.column_stack((pos_xs, pos_ys))
     initial_headings = pos_hs
     simulate_swarm(n_agents, initial_positions, initial_headings)
